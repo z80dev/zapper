502a375202576832bd041a23a3f25366922ba69e
z <dev@millenial.finance>
2021-05-16 17:33:53 -0400

    Update IZap

--- contracts/IZap.sol
+++ contracts/IZap.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.6.12;
 
 interface IZap {
     function zapOut(address _from, uint amount) external;
+    function zapOutToken(address _from, uint amount, address _to, address routerAddr) external;
     function zapIn(address _to) external payable;
     function zapInToken(address _from, uint amount, address _to) external;
+    function zapInTokenToVault(address _from, uint amount, address _to, address routerAddr, address _vault) external;
+    function zapInToVault(address _to, address routerAddr, address _vault) external payable;
+    function zapAcross(address _from, uint amount, address _fromRouter, address _toRouter) external;
 }
