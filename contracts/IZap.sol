b172d99f74a6613e829a939e15865c8e2685c9cf
z <dev@millenial.finance>
2021-05-16 17:40:24 -0400

    Add zapToFarm to IZap

--- contracts/IZap.sol
+++ contracts/IZap.sol
@@ -8,5 +8,6 @@ interface IZap {
     function zapInToken(address _from, uint amount, address _to) external;
     function zapInTokenToVault(address _from, uint amount, address _to, address routerAddr, address _vault) external;
     function zapInToVault(address _to, address routerAddr, address _vault) external payable;
+    function zapInToFarm(address _to, address routerAddr, address _masterChef, uint _pid) external payable;
     function zapAcross(address _from, uint amount, address _fromRouter, address _toRouter) external;
 }
